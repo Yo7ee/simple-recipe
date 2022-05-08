cb4253bea24891a5f7ce877d1e838ce56ad48878
yo7ee <yo7chen@gmail.com>
2022-05-08 22:20:32 +0800

    add signin/signup route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import HomePage from "./homePage";
 import Signin from "./signin/signin";
+import Signup from "./signin/signup";
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Recipe from "./Recipe/Recipe";
 
@@ -11,6 +12,7 @@ function App(){
             <Routes>
                 <Route path="/" element={<HomePage/>}/>
                 <Route path="/signin" element={<Signin/>}/>
+                <Route path="/signup" element={<Signup/>}/>
                 <Route path="/recipe" element={<Recipe/>}/>
             </Routes>
         </BrowserRouter>
